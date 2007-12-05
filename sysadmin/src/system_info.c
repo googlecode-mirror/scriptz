258a40aa00196dc31a7345aa318d2fbda955f578
fred1982 <fred1982@f8e68f4b-7140-0410-b3cc-a1ab6a34cf7c>
2007-12-05 13:32:19 +0000

    Add system_info.c file

---
+++ sysadmin/src/system_info.c
@@ -0,0 +1,11 @@
+/**
+ * $Id$
+ *
+ * This file is a C programme and it will display information of 
+ * 	Linux/Unix system, use CGIC
+ * 		http://www.boutell.com/cgic/
+ * 	
+ * Sysadmin is a project of Fred Chu <zhuzhu@Plerchina.org>
+ * you can get new newest source on 
+ * 		http://scriptz.googlecode.com/svn/trunk/sysadmin/ 
+ */
