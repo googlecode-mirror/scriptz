75a150210f0406ddcee48068a6eab18a714c865e
fred1982 <fred1982@f8e68f4b-7140-0410-b3cc-a1ab6a34cf7c>
2007-12-20 13:30:36 +0000

    webcheck.pl add mysql mysql -uroot -pxxx --execute="show full processlist" report

--- scripts/webcheck.pl
+++ scripts/webcheck.pl
@@ -104,5 +104,5 @@ sub CheckLoadavg {
 
 sub SendMail {
 	my $msg = shift;
-	system("export LANG=c && echo \"Date: `date +%F\\ %r `\n\n `w` \n\n `ps auxf` \n\n `lynx http://127.0.0.1/server-status -dump`\" | mail -s \"$msg\" $mailAddress");
+	system("export LANG=c && echo \"Date: `date +%F\\ %r `\n\n `w` \n\n `ps auxf` \n\n `lynx http://127.0.0.1/server-status -dump` \n\n `mysql -uroot -p123456 --execute=\"show full processlist\"`\" | mail -s \"$msg\" $mailAddress");
 }
