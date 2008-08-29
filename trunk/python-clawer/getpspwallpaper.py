5fbc035cb0a3479e58ce49bc9ff8c57491b01caa
fred1982 <fred1982@f8e68f4b-7140-0410-b3cc-a1ab6a34cf7c>
2008-08-29 10:25:19 +0000

    Python Clawer Classs adn Modle

---
+++ trunk/python-clawer/getpspwallpaper.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+# $Id$
+#
+import pycurl
+import StringIO
+import time
+import datetime
+import re
+import sys. os
+import urllib
+import stat
+import md5
+import random
+from BeautifulSoup import BeautifulSoup, Comment
+
+# build a crwaler first
