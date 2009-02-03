e6d3684e7d9939f91d3bbe4c99497587e8557eb2
fred1982 <fred1982@f8e68f4b-7140-0410-b3cc-a1ab6a34cf7c>
2009-02-03 02:57:03 +0000

    init python in maesinfo

---
+++ trunk/maesinfo-python/rsync-1ting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# $Id$
+# Download music file form 1ting.com
+# fred@webcan.cn
+# Get best editor "Emacs" on http://www.gnu.org
+# FreeSoft, Opensource is everything.
+# Visit http://www.linuxpk.com get more about Linux.
+# Use Python! http://www.python.org
+# Best music site http://www.1ting.com
